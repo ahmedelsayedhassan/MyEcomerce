7717ec801e8de8bec09b7b0f0ada97ace81d8c73
Ahmed_Sarhan <45242286+ahmedelsayedhassan@users.noreply.github.com>
2023-02-11 11:30:31 +0200

    Delete Ecommerce directory

--- Ecommerce/Ecommerce.PL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ecommerce.PL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
