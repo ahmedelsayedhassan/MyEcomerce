5f4319f1b606287b2df982472f966f60a3aee555
Ahmed_Sarhan <45242286+ahmedelsayedhassan@users.noreply.github.com>
2023-02-11 11:27:08 +0200

    Add files via upload

---
+++ Ecommerce/Ecommerce.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
